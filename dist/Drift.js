a24b04e6d757b16dc9cf02f1ad884390bda9e908
Paul Straw <paulstraw@paulstraw.com>
2016-01-16 19:02:22 -0800

    Bump version to 0.1.4

--- dist/Drift.js
+++ dist/Drift.js
@@ -27,7 +27,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var VERSION = exports.VERSION = '0.1.3';
+var VERSION = exports.VERSION = '0.1.4';
 
 var Drift = (function () {
   function Drift(triggerEl) {
