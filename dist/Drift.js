2fa079c7092379f432cd56aaf4c26a75fcb55846
Paul Straw <paulstraw@paulstraw.com>
2016-07-12 16:58:31 -0700

    Bump version to 0.3.1

--- dist/Drift.js
+++ dist/Drift.js
@@ -144,7 +144,7 @@ module.exports = function () {
 
     _classCallCheck(this, Drift);
 
-    this.VERSION = '0.3.0';
+    this.VERSION = '0.3.1';
 
     this.destroy = function () {
       _this.trigger._unbindEvents();
