965b8b2c25c15821e4076a9f8a10df1666f74ce1
Frederick Fogerty <frederick.fogerty@gmail.com>
2018-06-09 11:08:35 +1200

    Reset drift to master

--- dist/Drift.js
+++ dist/Drift.js
@@ -301,8 +301,6 @@ module.exports = function () {
     set: function set(zf) {
       this.settings.zoomFactor = zf;
       this.zoomPane.settings.zoomFactor = zf;
-      this.trigger.settings.zoomFactor = zf;
-      this.boundingBox.settings.zoomFactor = zf;
     }
   }]);
 
