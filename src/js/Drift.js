70367a36289b673056abb86670408e5723f3b760
imgix-git-robot <support@imgix.com>
2022-10-10 19:22:58 +0000

    chore(release): 1.5.1
    ## [1.5.1](https://github.com/imgix/drift/compare/v1.5.0...v1.5.1) (2022-10-10)
    
    ### Bug Fixes
    
    * add required test for eventlistener ([c43f12e](https://github.com/imgix/drift/commit/c43f12eb9b9bcb2fbbbc8edfc4e3cfd94bb2f0b0)), closes [#647](https://github.com/imgix/drift/issues/647)
    * remove optional eventlistener parameters ([d546ce1](https://github.com/imgix/drift/commit/d546ce1f5ed4260d698ecd46be2a092221630d55)), closes [#647](https://github.com/imgix/drift/issues/647)
    
     [skip ci]

--- src/js/Drift.js
+++ src/js/Drift.js
@@ -6,7 +6,7 @@ import ZoomPane from "./ZoomPane";
 
 export default class Drift {
   constructor(triggerEl, options = {}) {
-    this.VERSION = "1.5.0";
+    this.VERSION = "1.5.1";
     this.triggerEl = triggerEl;
 
     this.destroy = this.destroy.bind(this);
